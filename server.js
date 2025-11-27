bbe3188d9d4c82adfe2ae52599269fc5d22dd730
JeevaMatrix <jeevanandh510@gmail.com>
2025-11-27 18:35:35 +0530

    first

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: 'http://localhost:3000', // or your frontend domain
+  origin: 'https://hackhub-now.netlify.app/', // or your frontend domain
   credentials: true,
 }));
 
